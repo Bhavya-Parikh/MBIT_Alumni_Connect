0248ea6f55b3695586e5695d150ecd6479134542
Bhavya-Parikh <bhavyaparikh0@gmail.com>
2022-10-30 00:20:41 +0530

    moved login to auth folder

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,11 +1,11 @@
-const Students = require('../../models/student')
+const s = require('../../models/student')
 const bcrypt = require('bcrypt')
 const passport = require('passport')
 
 function authController(){
     return {
     login(req, res) {
-        res.render('login')
+        res.render('auth/login')
     }
 }
 }
